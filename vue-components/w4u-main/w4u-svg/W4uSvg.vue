5034f8d34b6c5125db275253a61ce9fa0172381f
Unknown <michael.kahle@yahoo.de>
2019-09-04 07:37:11 +0200

    Add Svg-Component

---
+++ vue-components/w4u-main/w4u-svg/W4uSvg.vue
@@ -0,0 +1,38 @@
+<template>  
+  <div class='w4uSvg'>
+  <svg id='w4uSvg' ref='w4uSvg'></svg>
+  <slot></slot>
+ </div> 
+  </div>
+</template>
+ 
+<style scoped>
+  .w4uSvg { 
+    background: green;
+  }
+</style>
+
+<script>
+  import C4uGlue from "c4u-glue";
+  import SnapSvg from "snapsvg-cjs";
+  import SnapFoo from "snapfoo";
+ 
+  export default {
+    data: function() {
+           return {
+            c4uParentTag: "c4u-also-to-be-defined",  // no parent tag
+            }
+        },
+    mixins: [C4uGlue], 
+
+    mounted() {
+         console.log("*****  SVG mounted #" + this.c4uUid);
+         var svg = this.$refs.w4uSvg;
+         var s = Snap(svg);
+         var bigCircle = s.circle(150, 150, 100);
+         bigCircle.animate({r: 50}, 1000);
+         
+    },
+
+  }
+</script>
---
+++ vue-components/w4u-main/w4u-svg/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash 
+
+npm install --save snapsvg-cjs
+npm install --save snapfoo
+
+#http://snapsvg.io/  && https://www.npmjs.com/package/snapsvg-cjs
+#http://yuschick.github.io/SnapFoo/
+
+
