ae115d99740981b4e57376d41f9bc61f5e81a5ba
Unknown <michael.kahle@yahoo.de>
2019-09-20 22:23:31 +0200

    Add component for calculation

---
+++ vue-components/w4u-main/w4u-math/W4uEquation.vue
@@ -0,0 +1,75 @@
+<template>  
+  <div class='w4uEquation'>
+  <div id='w4uEquation' ref='w4uEquation'>
+     <div id='w4uMath1' ref='w4uMathPretty'>{{ pretty }} </div>
+     <div id='w4uMath2' ref='w4uMathResult'>{{ result }} </div>
+  </div>
+  <slot></slot>
+ </div> 
+</template>
+  
+<style scoped>
+  .w4uMath { 
+    background: orange;
+  }
+</style>
+
+<script>
+  import C4uGlue from "c4u-glue";
+ 
+  import { create, all } from 'mathjs'
+  const config = { }
+  const math = create(all, config)
+
+
+  //import MathJax from "mathjax3";
+  //import MathJax from "mathjax";
+
+  export default {
+    props: {
+            output:   {type: String, default: 'y0'},          
+            inputs:   {type: String, default: 'x0,z0'},   
+            formula:  {type: String, default: 'sqrt(x0)*sin(z0)'},   
+           }, 
+    data: function() {
+           return {
+            c4uParentTag: "w4u-math", 
+	    pretty: '$$$$',
+            }
+        },
+   mixins: [C4uGlue],
+   methods: { 
+        calculate(parent) {
+           var resultValue = math.format(math.evaluate(this.formula, this.w4uAllInputs));
+           Vue.set(parent.w4uOutputs, this.output, { 'value': resultValue, 'time': 0.0 });
+        }, 
+    },
+    computed: {
+      w4uAllInputs: function() {
+            var allEquationInputs = {};
+            var inputs = this.inputs.split(',');
+            for(var i=0; i<this.inputs.length; i++) {
+               var input = inputs[i];
+               if (input in this.c4uParent.w4uInputs) {
+                  allEquationInputs[input] = this.c4uParent.w4uInputs[input].value;
+               }
+            }
+            return allEquationInputs;
+      }
+    }, 
+    mounted() {
+         console.log("*****  Math mounted #" + this.c4uUid);
+
+          // this.result = math.format(math.evaluate(this.formula, {x0: 5, z0: 4})); 
+	  var latex = math.parse(this.formula).toTex({parenthesis: 'auto', implicit: 'hide'});
+	  console.log('LaTeX expression:', latex)
+
+          var el = this.$refs.w4uMathPretty;
+	  const elem = MathJax.Hub.getAllJax(el)[0];
+	  MathJax.Hub.Queue(['Text', elem, latex]);
+       
+         
+    },
+
+  }
+</script>
--- vue-components/w4u-main/w4u-math/W4uMath.vue
+++ vue-components/w4u-main/w4u-math/W4uMath.vue
@@ -1,45 +1,105 @@
 <template>  
   <div class='w4uMath'>
   <div id='w4uMath' ref='w4uMath'>
-     <div id='w4uMath1' ref='w4uMathPretty'>{{ pretty }} </div>
-     <div id='w4uMath2' ref='w4uMathResult'>{{ result }} </div>
   </div>
+  <w4u-io v-bind:name="name" v-bind:inputs="w4uStringIn" v-bind:outputs="w4uStringOut" > </w4u-io>
   <slot></slot>
  </div> 
 </template>
   
 <style scoped>
-  .w4uSvg { 
+  .w4uMath { 
     background: orange;
   }
 </style>
 
 <script>
+  import W4uIo from "w4u-io";
   import C4uGlue from "c4u-glue";
- 
-  import { create, all } from 'mathjs'
-  const config = { }
-  const math = create(all, config)
-
-
-  //import MathJax from "mathjax3";
-  //import MathJax from "mathjax";
 
   export default {
+    props: {
+            name: {type: String, default: 'math0'},          //automatic numbering would need glue for unique id...
+           },
     data: function() {
            return {
             c4uParentTag: "c4u-also-to-be-defined",  // no parent tag
-            form0: '5 sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / 4)^2',
+            form0: '5 sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / x1)^2',
 	    pretty: '$$$$',
-	    result: '???'
+	    result: '???',
+             w4uInputs:  { 'x1': {'value': 0.0, 'time':0.0 },   // will later be derrived from children 
+                           'x2': {'value': 0.0, 'time':0.0 },
+                           'x3': {'value': 0.0, 'time':0.0 },   
+                           'x4': {'value': 0.0, 'time':0.0 },
+                         },
+             w4uOutputs: { 'y1': {'value': 0.5, 'time':0.0 },  // will later be derrived from children 
+                           'y2': {'value': 0.5, 'time':0.0 },
+                           'y3': {'value': 0.5, 'time':0.0 },
+
+                         }
             }
         },
-    mixins: [C4uGlue], 
-
+   mixins: [W4uIo, C4uGlue],
+    methods: {
+        c4uChildDisconnected(child) { this.revaluateEquations(); },   // can be overwritten by parent
+        c4uChildReconnected(child) { this.revaluateEquations(); },
+        revaluateEquations() {
+          var newInputs = {};
+          var newOutputs = {};
+          if(this.c4uChildren['w4u-equation']) {
+          for(var i=0; i<(this.c4uChildren['w4u-equation'].length); i++) {
+            var equation = this.c4uChildren['w4u-equation'][i];
+            var inputs = equation.inputs.split(',');
+            for(var j=0; j<inputs.length; j++) {
+               var input = inputs[j];
+               if (input in this.w4uInputs) {
+                  newInputs[input] = this.w4uInputs[input];
+               } else {
+                  newInputs[input] = { 'value': 0.0, 'time':0.0 };
+               }
+            }
+            var ouput = equation.output;
+            if (ouput in this.w4uOutputs) {
+               newOutputs[ouput] = this.w4uOutputs[ouput];
+            } else {
+               newOutputs[ouput] = { 'value': 0.0, 'time':0.0 };
+            }
+          }
+          }
+          this.w4uOutputs = newOutputs;
+          this.w4uInputs = newInputs;
+        },
+        recalculateEquations() {
+          if(this.c4uChildren['w4u-equation']) {
+            for(var i=0; i<(this.c4uChildren['w4u-equation'].length); i++) {
+              var equation = this.c4uChildren['w4u-equation'][i];
+              equation.calculate(this);
+            }
+          }
+        },
+    },
+    watch: {
+      w4uAllEquations: function (newValue) {
+         this.revaluateEquations();
+      },
+/*
+      w4uInputs: function(newValue) {
+         this.recalculateEquations();
+      },
+*/
+      w4uStringIn: function(newValue) {
+         this.recalculateEquations();
+      },
+    }, 
+    computed: {
+      w4uAllEquations: function() {
+         return this.c4uChildren['w4u-equation'];
+      }
+    },  
     mounted() {
          console.log("*****  Math mounted #" + this.c4uUid);
 
-          this.result = math.format(math.evaluate(this.form0));
+          this.result = math.format(math.evaluate(this.form0, {x1: 5}));
 	  var latex = math.parse(this.form0).toTex({parenthesis: 'auto', implicit: 'hide'});
 	  console.log('LaTeX expression:', latex)
 /*
