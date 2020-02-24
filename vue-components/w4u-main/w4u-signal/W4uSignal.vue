1d69ef1af125b763de617db58012f596d5d93804
Unknown <michael.kahle@yahoo.de>
2020-02-24 22:53:09 +0100

    Add noise

--- vue-components/w4u-main/w4u-signal/W4uSignal.vue
+++ vue-components/w4u-main/w4u-signal/W4uSignal.vue
@@ -21,29 +21,34 @@
             name: {type: String, default: 'signal0'},          //automatic numbering would need glue for unique id...
             frequency: {type: Number, default: 1.0}, 
             amplitude: {type: Number, default: 1.0},
+            offset:    {type: Number, default: 0.0},
             interval:  {type: Number, default: 0.1},  
             play:      {type: Number, default: 1.0},
            },
     data: function() {
            return {
-             c4uParentTag: "w4u-stage",   //maybe kind of page later // or handling for none-parent -> window.root
+             c4uParentTag: "c4u-circuitry",   
              w4uTimer: null, 
              w4uRamp:  0.0,
-             w4uRadian: 0.0,  
-             w4uOutputs: { 'ramp': {'value': 0.5, 'time':0.0 },
-                           'sin':  {'value': 0.5, 'time':0.0 },
-                           'cos':  {'value': 0.5, 'time':0.0 },
+             w4uRadian: 0.0, 
+             w4uNoise: 0.0, 
+             w4uOutputs: { 'ramp':  {'value': 0.0, 'time':0.0 },
+                           'sin':   {'value': 0.0, 'time':0.0 },
+                           'cos':   {'value': 0.0, 'time':0.0 },
+                           'noise': {'value': 0.0, 'time':0.0 },
 
                          },
              w4uInputs:  { 'frequency': {'value': this.frequency, 'time':0.0 },   // interval * frequency < 1!!
                            'amplitude': {'value': this.amplitude, 'time':0.0 },
+                           'offset':    {'value': this.offset, 'time':0.0 },
                            'interval':  {'value': this.interval,  'time':0.0 },   // interval > 0.001 ?
                            'play':      {'value': 1.0, 'time':0.0 },
                          }
             }
         },
    watch: { /*eslint no-unused-vars: ["error", { "args": "none" }]*/
-      w4uStringIn: function (newValue) { this.modifyElem(); }  
+      w4uStringIn: function (newValue) { this.modifyElem(); },
+      //amplitude: function (newValue) { this.w4uInputs['amplitude']['value'] = newValue; },  
    }, 
    mounted() {
        this.createElem();
@@ -58,6 +63,7 @@
         createElem() {
            this.deleteElem();
            this.w4uTimer = setInterval(this.update, 1000*this.w4uInputs.interval.value); //ms
+           //this.w4uInputs['amplitude']['value'] = this.amplitude;
 	},
         deleteElem() {
          if(this.w4uTimer) {
@@ -75,12 +81,14 @@
                 this.w4uRadian -= 2.0*Math.PI;
              }
              //console.log('Signal now #'+this.c4uUid.toString()+': '+this.w4uRamp.toString()); 
-             var sin = this.w4uInputs.amplitude.value*Math.sin(this.w4uRadian); 
-             var cos = this.w4uInputs.amplitude.value*Math.cos(this.w4uRadian); 
+             var sin = this.w4uInputs.amplitude.value*Math.sin(this.w4uRadian)+this.w4uInputs.offset.value; 
+             var cos = this.w4uInputs.amplitude.value*Math.cos(this.w4uRadian)+this.w4uInputs.offset.value; 
+             this.w4uNoise += 0.1*step*(Math.random()-0.5);  //arctan -1..+1
+             var noise = this.w4uInputs.amplitude.value*this.w4uNoise+this.w4uInputs.offset.value;
              Vue.set(this.w4uOutputs, 'ramp', {'value': this.w4uRamp, 'time': this.w4uInputs.interval.value});
              Vue.set(this.w4uOutputs, 'sin', {'value': sin, 'time': this.w4uInputs.interval.value});
              Vue.set(this.w4uOutputs, 'cos', {'value': cos, 'time': this.w4uInputs.interval.value});
-
+             Vue.set(this.w4uOutputs, 'noise', {'value': noise, 'time': this.w4uInputs.interval.value});
 	},
     },
 
