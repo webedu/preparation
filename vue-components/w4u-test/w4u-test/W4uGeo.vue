a9049749f9f2d62ab71900d3522901824304b14c
Unknown <michael.kahle@yahoo.de>
2020-03-02 22:33:13 +0100

    normalize

--- vue-components/w4u-test/w4u-test/W4uGeo.vue
+++ vue-components/w4u-test/w4u-test/W4uGeo.vue
@@ -2,15 +2,8 @@
   <span class="w4uGeo">
     <w4u-io 
       v-bind:name="name" 
-      v-bind:inputs="w4uStringIn" 
       v-bind:outputs="w4uStringOut"
     />  
-    <h5>Position</h5>  
-    <ul>
-      <li>Latitude: {{ w4uOutputs.latitude.value }}</li>
-      <li>Longitude: {{ w4uOutputs.longitude.value }}</li>
-    </ul>
-    <slot />  
   </span> 
 </template>
 
@@ -25,15 +18,12 @@
     props: {
             name:     {type: String, default: 'geo0'},          //automatic numbering would need glue for unique id...
             mode:     {type: String, default: 'watch'},         //"watch"|"once"
-            normalize: {type: Boolean, default: true},          //0..1
-            accuracy: {type: Number, default: 0.0},
+            degree:   {type: Boolean, default: false},          // 0..360 instead 0..1
            },
     data: function() {
            return {
              c4uParentTag: "c4u-circuitry",   
-             w4uLatitude: 0,
-             w4uLongitude: 0,
-             w4uWatchId: null,
+             w4uWatchTId: null,
              w4uOutputs: { 'latitude':    {'value': 0.0, 'time':0.0 },
                            'longitude':   {'value': 0.0, 'time':0.0 },
                            'altitude ':   {'value': -1.0, 'time':0.0 },  // m
@@ -41,22 +31,20 @@
                            'speed':       {'value': -1.0, 'time':0.0 },  // m/s
                            'status':      {'value':  0.0, 'time':0.0 },  // 0:failure, 1:success
                          },
-             w4uInputs:  { 'frequency': {'value': this.frequency, 'time':0.0 },   // interval * frequency < 1!!
-                           'amplitude': {'value': this.amplitude, 'time':0.0 },
-                           'offset':    {'value': this.offset, 'time':0.0 },
-                           'interval':  {'value': this.interval,  'time':0.0 },   // interval > 0.001 ?
-                           'play':      {'value': 1.0, 'time':0.0 },
-                         }
             }
         },
-   watch: { /*eslint no-unused-vars: ["error", { "args": "none" }]*/
-      //amplitude: function (newValue) { this.w4uInputs['amplitude']['value'] = newValue; },  
-   }, 
    mounted() {
      this.initPosition();
    },
-   methods: { 
+   methods: { /*eslint no-unused-vars: ["error", { "args": "none" }]*/
+        c4uParentDisconnected(parent) {
+           this.stopPosition();
+        }, /*eslint no-unused-vars: ["error", { "args": "none" }]*/
+        c4uParentReconnected(parent) {
+           this.initPosition();
+        },
       initPosition() {
+       this.stopPosition();
        inqPosition(this, Vue)
         .then((result) => {
           result.vueGeo.updatePosition(result.position);
@@ -65,7 +53,7 @@
           result.vueGeo.failPosition(result.error);
         });
        if('watch'==this.mode) {
-         this.w4uWatchId = trackPosition(this, Vue)
+         trackPosition(this, Vue)
           .then((result) => {
             result.vueGeo.updatePosition(result.position);
           })
@@ -74,17 +62,35 @@
           });
         }
       },
+      stopPosition() {
+        if(this.w4uWatchId) {
+          navigator.geolocation.clearWatch(this.w4uWatchId);
+          this.w4uWatchId = null;
+        }
+      },
       updatePosition(position) {
-           this.w4uLatitude = position.coords.latitude;
-           this.w4uLongitude = position.coords.longitude;
-           Vue.set(this.w4uOutputs, 'status', {'value': 1.0, 'time': 0.1});
-           Vue.set(this.w4uOutputs, 'latitude', {'value': position.coords.latitude, 'time': 0.1});
-           Vue.set(this.w4uOutputs, 'longitude', {'value': position.coords.longitude, 'time': 0.1});
-           if(position.coords.altitude) {
-             Vue.set(this.w4uOutputs, 'altitude', {'value': position.coords.altitude, 'time': 0.1});
+           var latitude = position.coords.latitude;
+           var longitude = position.coords.longitude;
+           if (!this.degree) {
+             latitude = latitude/90.0;
+             longitude = longitude/180.0;
            }
-           if(position.coords.heading) {
-             Vue.set(this.w4uOutputs, 'heading', {'value': position.coords.heading, 'time': 0.1});
+           Vue.set(this.w4uOutputs, 'status', {'value': 1.0, 'time': 0.1});
+           Vue.set(this.w4uOutputs, 'latitude', {'value': latitude, 'time': 0.1});
+           Vue.set(this.w4uOutputs, 'longitude', {'value': longitude, 'time': 0.1});
+           var altitude = position.coords.altitude;
+           if(altitude) {
+             if (!this.degree) {
+               altitude = altitude/10000.0;
+             }
+             Vue.set(this.w4uOutputs, 'altitude', {'value': altitude, 'time': 0.1});
+           }
+           var heading = position.coords.heading;
+           if(heading) {
+             if (!this.degree) {
+               heading = heading/360.0;
+             }
+             Vue.set(this.w4uOutputs, 'heading', {'value': heading, 'time': 0.1});
            }
            if(position.coords.speed) {
              Vue.set(this.w4uOutputs, 'speed', {'value': position.coords.speed, 'time': 0.1});
@@ -110,7 +116,8 @@ var inqPosition = function(vueGeo) {
         },
         function(error) {
           reject({'vueGeo': vueGeo, 'error': error});
-        }
+        },
+        {'timeout':5000, 'maximumAge':10000}
      );
   });
 }
@@ -118,15 +125,15 @@ var inqPosition = function(vueGeo) {
 var trackPosition = function(vueGeo) {
   return new Promise(
     function(resolve, reject) {
-      var watchId = navigator.geolocation.watchPosition(
+      vueGeo.w4uWatchId = navigator.geolocation.watchPosition(
         function(position) {
           resolve({'vueGeo': vueGeo, 'position': position});
         },
         function(error) {
           reject({'vueGeo': vueGeo, 'error': error});
-        }
+        },
+        {'timeout':5000, 'maximumAge':10000}  //5s,10s
      );
-     return watchId;
   });
 }
 
@@ -137,13 +144,3 @@ var trackPosition = function(vueGeo) {
 </style>
 
 
-
-// https://www.w3schools.com/html/html5_geolocation.asp
-
-//latitude
-//longitude
-//attitude
-//speed
-//heading
-
-//watchPosition()
