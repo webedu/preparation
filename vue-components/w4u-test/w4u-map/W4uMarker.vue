b53569e63141e2778d18095b58310d3e2e949bed
Unknown <michael.kahle@yahoo.de>
2020-03-19 20:23:14 +0100

    Add tooltip

--- vue-components/w4u-test/w4u-map/W4uMarker.vue
+++ vue-components/w4u-test/w4u-map/W4uMarker.vue
@@ -34,6 +34,7 @@
             latitude:  {type: Number, default: 0.0}, 
             longitude: {type: Number, default: 0.0},
             zoom:      {type: Number, default: 0.5},          // zoom is used, when marker is activated 
+            tooltip:   {type: String, default: ''}, 
           //  rotation:  {type: Number, default: 0.0},        // rotation not working - for font-awesome?
             opacity:   {type: Number, default: 1.0},
             draggable: {type: Boolean, default: false},
@@ -93,7 +94,7 @@ if(this.$el && this.c4uParent && this.c4uParent.w4uCluster) {
                       prefix: 'fa', svg: false};
           var marker =  L.ExtraMarkers.icon(data);
           this.w4uMarker = L.marker( [90.0*this.w4uInputs.latitude.value, 180.0*this.w4uInputs.longitude.value],
-                                     {icon: marker, title: 'Hover', opacity: this.w4uInputs.opacity.value, 
+                                     {icon: marker, title: this.tooltip, opacity: this.w4uInputs.opacity.value, 
                                       draggable: false, autoPan: false} );  
           // https://leafletjs.com/reference-1.6.0.html#marker
           // this.w4uMarker = L.marker( [48, 8] );
